10909bd37fb01a9cf6bc31061d04a0f2b5ce6c74
VladislavGarkun <giannis_34@mail.ru>
2024-02-17 18:49:57 +0100

    updated appspec and script files

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ source ~/.bashrc
 # To kill the process incase of redeployment
 kill -9 $(lsof -i :8080)
 cd /home/ec2-user/my-web-server/build/libs/
-nohup java -jar spring-web-server-0.0.1-SNAPSHOT.jar &> webserverlog.log &
+nohup java -jar aws-deployment.jar &> webserverlog.log &
