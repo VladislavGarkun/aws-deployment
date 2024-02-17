f97778890a0226c605b1d3e5c1b1ae233a0c916e
VladislavGarkun <giannis_34@mail.ru>
2024-02-17 19:07:49 +0100

    updated script path

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 source ~/.bashrc
 # To kill the process incase of redeployment
 kill -9 $(lsof -i :8080)
-cd /home/ec2-user/my-web-server/build/libs/
+cd /home/ec2-user/my-web-server/
 nohup java -jar aws-deployment.jar &> webserverlog.log &
