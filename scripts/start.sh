1777b7892d19caa3d74805ffd2386804f542d053
VladislavGarkun <giannis_34@mail.ru>
2024-02-18 20:49:15 +0100

    updated start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 
-if [ "$DEPLOYMENT_GROUP_NAME" == "billing" ]
-then
-    nohup java -jar -Dspring.profiles.active=dev /tmp/billing-service.jar > /dev/null 2> /dev/null < /dev/null &
-else
-    nohup java -jar -Dspring.profiles.active=dev /tmp/order-service.jar > /dev/null 2> /dev/null < /dev/null &
-fi
-#else
-#  echo "No running applications was found for the deployment group name!"
-#fi
-
+nohup java -jar -Dspring.profiles.active=dev /tmp/aws-deployment.jar > /dev/null 2> /dev/null < /dev/null &
 exit 0
