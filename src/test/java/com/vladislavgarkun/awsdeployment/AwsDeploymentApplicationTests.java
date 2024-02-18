2c8efccd10c0a6c6da11afe2dfe9e3497d873e50
VladislavGarkun <giannis_34@mail.ru>
2024-02-18 12:04:35 +0100

    added test with error

--- src/test/java/com/vladislavgarkun/awsdeployment/AwsDeploymentApplicationTests.java
+++ src/test/java/com/vladislavgarkun/awsdeployment/AwsDeploymentApplicationTests.java
@@ -1,5 +1,6 @@
 package com.vladislavgarkun.awsdeployment;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class AwsDeploymentApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(4).isEqualTo(3);
 	}
 
 }
