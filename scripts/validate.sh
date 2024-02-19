97f7cc97481551018b960612373168e443c4da4b
VladislavGarkun <giannis_34@mail.ru>
2024-02-19 13:45:35 +0100

    updated validation again

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -2,7 +2,7 @@
 
 while 'true'
 do
-    if [ "$(curl -s http://localhost:8080/actuator/health)" = '{"status":"UP"}' ]
+    if [ "$(curl -s http://localhost:8080/health)" = '{"status":"OK"}' ]
     then
         echo "Application is running!"
         exit 0
