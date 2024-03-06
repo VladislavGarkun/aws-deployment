82d63775998fdcc8393d86f6105c922b60e3e51a
VladislavGarkun <giannis_34@mail.ru>
2024-03-06 16:07:41 +0100

    changed path for copy file and run script

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,5 +4,5 @@ echo "Uploading Cloudwatch configuration..."
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/tmp/amazon-cloudwatch-agent.json
 
 echo "Trying to run application..."
-nohup java -jar -Dspring.profiles.active=dev /tmp/aws-deployment.jar > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar -Dspring.profiles.active=dev /tmp/target/aws-deployment.jar > /dev/null 2> /dev/null < /dev/null &
 exit 0
