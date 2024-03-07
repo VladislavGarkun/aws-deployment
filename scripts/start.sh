9dd82879032c12039a3622119884fcd99eb63f3d
VladislavGarkun <giannis_34@mail.ru>
2024-03-07 14:03:43 +0100

    changed path for amazon-cloudwatch-agent.json

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Uploading Cloudwatch configuration..."
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/tmp/.aws/amazon-cloudwatch-agent.json
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/tmp/amazon-cloudwatch-agent.json
 
 echo "Trying to run application..."
 nohup java -jar -Dspring.profiles.active=dev /tmp/target/aws-deployment.jar > /dev/null 2> /dev/null < /dev/null &
