6c9386bcb5360df7b588acc9daab195359a82274
VladislavGarkun <giannis_34@mail.ru>
2024-04-01 15:01:38 +0200

    improved controller

--- src/main/java/com/vladislavgarkun/awsdeployment/HelloController.java
+++ src/main/java/com/vladislavgarkun/awsdeployment/HelloController.java
@@ -6,6 +6,9 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
 @Slf4j
 @RestController
 public class HelloController {
@@ -13,10 +16,13 @@ public class HelloController {
     @Value("${operating.system}")
     private String os;
 
+    @Value("${ec2}")
+    private String ec2;
+
     @GetMapping(value = "/health", produces = MediaType.APPLICATION_JSON_VALUE)
     public String getHelloMessage(){
         log.info("Health request is processed");
-        return "Hello from " + os + " EC2";
+        return "Hello from " + os + " " + ec2;
     }
 
 }
